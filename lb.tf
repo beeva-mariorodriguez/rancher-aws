06c985e0eb9348c88da77065811ef131e2c7f3cc
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-09-29 14:21:29 +0200

    add healthcheck to LB

--- lb.tf
+++ lb.tf
@@ -1,13 +1,18 @@
 resource "aws_elb" "lb" {
   internal = true
   subnets  = ["${aws_subnet.servers.id}"]
-
   listener {
     instance_port     = 8080
     instance_protocol = "http"
     lb_port           = 8080
     lb_protocol       = "http"
   }
-
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8080/health"
+    interval            = 30
+  }
   instances = ["${aws_instance.rancher_server.*.id}"]
 }
